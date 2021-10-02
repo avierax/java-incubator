5690afc9a31ea5bb5fd838863df5df63bc965ed1
ariel <ariel.viera@gmail.com>
2021-10-02 13:26:10 -0400

    Rename Excercise 138

--- cracking-the-code-interview/src/avierax/ctci/chapter13/Exercise138.java
+++ cracking-the-code-interview/src/avierax/ctci/chapter13/Exercise138.java
@@ -1,18 +1,21 @@
-package com.company;
+package avierax.ctci.chapter13;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-public class Main {
+/**
+ * Using lambda expressions, write a function List<Integer> that returns a random subset of arbitrary size.
+ */
+public class Exercise138 {
 
-    public static <T> List<T> randomSubset(List<T> list) {
+    public static List<Integer> getRandomSubset(List<Integer> list) {
         Random random = new Random();
         return list.stream().filter(t -> random.nextBoolean()).toList();
     }
 
     public static void main(String[] args) {
             var list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-            randomSubset(list).forEach(System.out::println);
+            getRandomSubset(list).forEach(System.out::println);
     }
 }
